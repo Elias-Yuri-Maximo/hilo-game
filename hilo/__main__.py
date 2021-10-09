cb44b7796c9546ea35ff77d31c4820f6e41dd94a
unknown <sumtwelve@gmail.com>
2021-10-09 14:42:00 -0600

    noah added documentation to participant.py and minor changes throughout. see slack.

--- hilo/__main__.py
+++ hilo/__main__.py
@@ -1,7 +1,9 @@
-# TODO: Add entry point code here
-
 from game.director import Director
 
+print("Welcome to High/Low!")
+print("Ready to risk it all? Guess whether the next card will be higher or lower to win points!")
+print("You lose if your score becomes zero!")
+print("Here's 300 points to start. Good luck!\n")
 
 director = Director()
 director.start_game()
--- hilo/game/director.py
+++ hilo/game/director.py
@@ -34,15 +34,15 @@ class Director:
             self.do_outputs()
 
     # def shuffle(self):
-    #    '''
+    #    """
     #    When called will generate a a random, non repeated,  list of numbers between 1-13
-    #    the list was already declared, so it should only append to it (list.append(item))'''
+    #    the list was already declared, so it should only append to it (list.append(item))"""
     #    random_list = random.sample(range(1, 14), 13)
     #    return random_list
 
     def get_inputs(self):
         """Gets the inputs at the beginning of each round of play. In this case,
-        that means throwing the dice.
+        that means shuffling the cards.
 
         Args:
             self (Director): An instance of Director.
@@ -50,50 +50,83 @@ class Director:
         self.participant.shuffle()
 
     def update_points(self):
-        '''
+        """
         Shows the card, asks the player and updates points.
         Receives the calculated points from participant
         and keeps track of the points
         Args:
             self(Director): an instance of Director.
-        '''
-        #print(f'This card is:{self.cards_list[self.current_round_index]}')
+        """
+        #print(f"This card is:{self.cards_list[self.current_round_index]}")
         # dealer prints current card
         #guess = input("is the next card higher or lower?(h/l)")
         # asks the player if they  think it is lower or higher
-        #print(f'Next card was:{self.cards_list[self.current_round_index + 1]}')
+        #print(f"Next card was:{self.cards_list[self.current_round_index + 1]}")
         # prints the next card.
 
         # makes this the current card.
         self.score = self.score + \
             (self.participant.calculate_points(
                 self.guess))
+
         self.current_round_index += 1
         # calculate_points() in participant should return the points FOR THE TURN
         # the participant calculates the score and it is added to current score,
         # which is then atributerd to current score.
 
     def do_outputs(self):
-        '''
+        """
         Prints the total score, calls the can_play function to to change the
         keep_playing value to False, if the player cannot play anymore.
         Args:
             self(Director): an instance of Director.
-        '''
+        """
         if self.participant.can_play(self.score):
-            print(f'This card is:{self.participant.card_value}')
 
-            # prints the score.
-            self.guess = input("is the next card higher or lower?(h/l)")
-            print(f'Next card was:{self.participant.card_new_value}')
+            # Ask for user input with a 'while True' to make sure they enter an acceptable selection
+            while True:
+                print(f"This card is: {self.participant.card_value}")
+
+                # Take user input and immediately convert it to lower case for easier comparison
+                self.guess = input("Is the next card higher or lower? (h/l) ").lower()
+                if self.guess in ["h", "l"]:
+                    break
+
+                # Don't need an else. This line will never print if above 'if' is true
+                print("Invalid selection.")
+
+            print(f"Next card was: {self.participant.card_new_value}")
             # attributes the players answer to choice.
             #self.keep_playing = self.participant.can_play(choice)
             # should can play should return a true boolean if the player
             # still wants to play, and if they have the points for it
             # if they don't have the points, return False.
+
+            # Save current score into a variable for comparison.
+            old_score = self.score
             self.update_points()
-            print(f'Your score is:{self.score}')
+
+            # Comparison made to tell user directly whether or not their guess was correct.
+            if self.score > old_score:
+                print("You guessed correctly! +100 points.")
+            else:
+                print("You guessed incorrectly. -75 points.")
+
+            print(f"Your score is: {self.score}")
             if self.score > 0:
-                choice = input('Would you like to play again (y,n)')
-                if (choice == "n"):
+                while True:
+                    choice = input("Would you like to play again? (y/n) ").lower()
+                    if choice in ["y", "n"]:
+                        break
+
+                    # Don't need an else-continue. This line will never print
+                    # if user enters 'y' or 'n':
+                    print("Invalid selection.")
+
+                print()
+                if choice.lower() == "n":
+                    print("Thanks for playing!")
                     self.keep_playing = False
+            else:
+                print("Game over!")
+                self.keep_playing = False
